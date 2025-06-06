054340568932d144219fb90db6991501ae6d6a23
Sebastián Camacho <96026758+YourDaddySebas@users.noreply.github.com>
2025-06-06 12:17:25 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ function App() {
   return (
     <div className="gta-map-container">
       <img
-        src="/images/jjbar.png"
+        src="images/jjbar.png"
         alt="J & J's Restaurant"
         className="img-folleto-jjs"
       />
       <img
-        src="/images/usfs-logo.png"
+        src="images/usfs-logo.png"
         alt="USFS"
         className="img-folleto-usfs"
       />
       
       <img
-        src="/images/smokey.png"
+        src="images/smokey.png"
         alt="Smokey Bear - El bosque es mi casa"
         className="img-folleto-smokey"
       />
